2990abc8c31537fdeb471f8819f17df018ac7038
Rhekneee <rodelas.reneemadel@ncst.edu.ph>
2025-05-02 14:30:11 +0800

    trippings one but without much detailed restrcition

--- backend/departments/crm/routes/crm.routes.js
+++ backend/departments/crm/routes/crm.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 // Import the correct controller and multer upload handler
-const { HRController, upload } = require("../controller/crm.controller");
+module.exports = { CRMController, upload }= require("../controller/crm.controller");
 
 // 🔹 Resume Upload Route
-router.post("/upload", upload.single("resume"), HRController.uploadResume);
+router.post("/upload", upload.single("resume"), CRMController.uploadResume);
+router.post('/submitVisitRequest', CRMController.createVisitRequest);
 
 module.exports = router;
