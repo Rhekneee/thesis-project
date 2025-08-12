abac4dd4dc85382d160a413422f5330b043d9e74
Rhekneee <rodelas.reneemadel@ncst.edu.ph>
2025-08-12 08:47:36 +0800

    changing for later

--- backend/server.js
+++ backend/server.js
@@ -176,6 +176,7 @@ app.get('/dashboard', (req, res) => {
         'foreman_3': '/manufacturing/manufacturing_dashboard',
         'admin_staff': '/hr_employee/attendance',
         'sales_marketing_head': '/crm admin/crm_admin.html',
+        'documentation_officer': '/crm admin/crm_admin.html',
         'logistics': '/scm admin/scm-dashboard.html',
         'agents': '/agents/agent_dashboard.html',
         // External user dashboards (using username)
