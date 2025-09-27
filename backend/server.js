4b80ce47f8db50be4fb2f0bbb488d8f772d4d443
Rhekneee <rodelas.reneemadel@ncst.edu.ph>
2025-09-28 00:30:39 +0800

    polishing manufacturing

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'views')));
-app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes
 app.use('/auth', authRoutes);
